2b2e7e370666a3316cb854c23e9cee551fda860e
SamLion <mailsonanacleto13@gmail.com>
2026-01-15 15:15:40 -0300

    Carregar e exibir lista de leitura a partir do localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
-  const [query, setQuery] = useState("");
-  const [books, setBooks] = useState([]);
-  const [error, setError] = useState("");
+  const [query, setQuery] = useState(""); //termo para busca dos livros
+  const [books, setBooks] = useState([]); //lista de livros buscados
+  const [listaLeitura, setListaLeitura] = useState([]); //livros escolhidos pelo usuário
+  const [error, setError] = useState(""); 
+
+  useEffect( () => {
+    const storedBooks = JSON.parse(localStorage.getItem("books")) || [];
+    setListaLeitura(storedBooks);
+  }, []);
 
   const buscarLivros = async () =>{
     if (!query) return; //retorna se a barra de pesquisa está vazia
@@ -24,6 +30,27 @@ function App() {
     
   }
 
+  const saveBook = (book) => {
+    const storedBooks = JSON.parse(localStorage.getItem("books")) || [];
+
+    const newBook = {
+      id: book.id,
+      titulo: book.volumeInfo.title,
+      autores: book.volumeInfo.authors,
+      capa: book.volumeInfo.imageLinks?.thumbnail,
+      totalPaginas: book.volumeInfo.pageCount,
+      status: "lendo",
+      registros: [],
+      resenhaFinal: "",
+      notaFinal: null
+    };
+
+    const updatedBooks = [...storedBooks, newBook]; // todos os livros salvos anteriormente + o livro a ser salvo
+
+    localStorage.setItem("books", JSON.stringify(updatedBooks)); // updatedBooks se torna a nova lista de livros
+    setListaLeitura(updatedBooks); // atualiza a lista de leitura na UI
+  };
+
   return (
     <div>
       <h1>Meu Diario Literario</h1>
@@ -32,14 +59,14 @@ function App() {
         buscarLivros();
         }}>
 
-        <input 
+        <input
           type="text" 
-          placeholder="Buscar livros ou autor"
+          placeholder="Buscar livro ou autor"
           value={query}
           onChange={(e) => setQuery(e.target.value)} 
         />
-
-        <button onClick={buscarLivros}>Buscar</button>
+        
+        <button onClick={buscarLivros}>Buscar</button> 
 
       </form>
 
@@ -53,9 +80,28 @@ function App() {
             />
             <p>{book.volumeInfo.title}</p>
             <p>{book.volumeInfo.authors?.join(", ")}</p>
+            <button onClick={() => saveBook(book)}>Adicionar</button>
           </li>
         ))}
       </ul>
+      
+      <div>
+        <h2>Lista de Leitura</h2>
+        
+        {listaLeitura === 0 && <p>Nenhum livro adicionado.</p>}
+
+        <ul>
+          {listaLeitura.map((book) => (
+            <li key={book.id}>
+              <img src={book.capa} alt={book.titulo} width="80" />
+              <p>{book.titulo}</p>
+              <p>{book.autores?.join(", ")}</p>
+            </li>
+          ))}
+        </ul>
+
+      </div>
+
     </div>
   )
 }
